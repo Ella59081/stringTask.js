d9e619e1e4f78d34706bb0a5178e5c030a466342
Ella59081 <elea123nor@gmail.com>
2025-03-06 08:44:40 +0100

    assignment

---
+++ right2.js
@@ -0,0 +1,14 @@
+// String-1 -- right2
+// Given a string, return a "rotated right 2" version where the last 2 chars are moved to the start. The string length will be at least 2.
+
+// Examples
+
+// right2('Hello') → loHel
+// right2('java') → vaja
+// right2('Hi') → Hi
+
+function right2(str){
+    return str.substr(str.length-2) + str.substr(0, str.length-2)
+}
+
+console.log(right2("hello"))
